1b9a055a23ccdb4d6164218fae81744b3eaf7280
Raphaela Dohrmann <raphaela.dohrmann@gfn.de>
2026-03-24 13:02:06 +0100

    Ergänzung verschachtelte Schleife

--- src/tag2/_06_For_Schleife.java
+++ src/tag2/_06_For_Schleife.java
@@ -8,20 +8,26 @@ public class _06_For_Schleife {
         // Sie wiederholt Anweisungen, solange eine Bedingung erfüllt ist.
         // Klassische for-Schleife: Startwert; Bedingung; Schrittweite
 
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             IO.println(i);
         }
 
-        // verschachtelten For-Schleifen
-        // äußere Schleife
-//        for (int i = 1; i <= 10; i++) {
-//            IO.println("Start der äußeren Schleife");
-//            for (int j = 1; j <= 5; j++) {
-//                IO.print(j);
-//            }
-//            IO.println();
-//            IO.println("Äußere nach Innerer");
-//        }
+        // Verschachtelte For-Schleifen
+        // Äußere for-Schleife:
+        // Diese Schleife bestimmt die Zeilen (z. B. 1 bis 3)
+        for (int i = 1; i <= 3; i++) {
+
+            // Innere for-Schleife:
+            // Diese Schleife läuft für jede Zeile erneut (z. B. 1 bis 4)
+            for (int j = 1; j <= 4; j++) {
+
+                // Ausgabe von i und j in einer Zeile
+                System.out.print("i=" + i + ", j=" + j + "  ");
+            }
+
+            // Zeilenumbruch nach jeder vollständigen inneren Schleife
+            System.out.println();
+        }
 
         // Enhanced For Loop
         String[] warenkorb = {"Laptop", "Buch", "Banane"};
